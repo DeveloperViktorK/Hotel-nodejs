d2a829a67687b9ef2843e721af7287670118aa74
Viktor <developerViktorK@yandex.ru>
2021-03-09 20:12:45 +0300

    test handlebars

--- hotelNode.js
+++ hotelNode.js
@@ -1,2 +1,17 @@
-console.log("test");
-let i = 0;
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
