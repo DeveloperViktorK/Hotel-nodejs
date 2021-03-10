984fd6bd17e9a2adfaa073deec854289e8cf930e
Viktor <developerViktorK@yandex.ru>
2021-03-10 22:46:44 +0300

    ya weather

--- hotelNode.js
+++ hotelNode.js
@@ -4,14 +4,18 @@ const app = express();
 const port = 3000;
 
 app.engine("handlebars", exphbs());
+
 app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
+const weatherMiddlware = require("./lib/middleware/weather");
+
+app.use(weatherMiddlware);
+
 app.get("/", (req, res) => {
   res.render("home");
 });
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
